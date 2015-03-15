586a6140668989e859ea03679acad4be240699e6
csemiao <csemiao@gmail.com>
2015-03-14 17:42:33 -0700

    why WONT YOU FIND THE LIBRARY ASKJDNASKDJASKDJHASKD

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -4,16 +4,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
-@RequestMapping("/")
+@RequestMapping(value = "/poops", method = RequestMethod.GET)
+
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model)
-    {
-		String theString = "haksdfjah";
-        model.addAttribute("theString", theString);
-        model.addAttribute("message", "Hello world!");
-		return "hello";
-	}
+
+    public ModelAndView getdata() {
+
+        List<String> list = getList();
+        ModelAndView model = new ModelAndView("hello");
+        model.addObject("listToOutput", list);
+
+        return model;
+
+    }
+
+    private List<String> getList() {
+
+        List<String> list = new ArrayList<String>();
+        list.add("poop");
+        list.add("poops");
+        list.add("more poops");
+        list.add("poopies");
+        list.add("more poopies");
+        list.add("even more poopies");
+        list.add("poop and poopies");
+
+        return list;
+
+    }
+
 }
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,6 +1,15 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
+<title>Mo money mo problems</title>
 <body>
-	<h1>${message}</h1>
-    <p> Bhawandeep. The String: ${theString} </p>
+    <table>
+        <ul>
+
+        <c:forEach var="item" items="${listToOutput}" >
+            <tr>
+                <td><c:out value="${item}" /></td>
+            </tr>
+        </c:forEach>
+    </table>
 </body>
 </html>
