c51f70a56936c7ea558b3ee8417e6b1a5a8e0a70
Paul Dante <pdante@users.noreply.github.com>
2015-03-15 01:21:13 -0700

    12 step compareto in Problem

--- src/main/java/Problem.java
+++ src/main/java/Problem.java
@@ -63,13 +63,37 @@ public class Problem implements Comparable<Problem> {
     public void setNotes(String notes) {
         this.notes = notes;
     }
-
+    /* this method should return -1 when problem 1 is sorted before problem 2.
+       0 when problem 1 is sorted equal to problem 2
+       +1 when problem 1 is sorted after problem 2
+       usage:  problem1.compareTo(problem2)                  */
     @Override
     public int compareTo(Problem problem2) {
-        /* TODO:  this method should return -1 when problem 1 is sorted before problem 2.
-                                            0 when problem 1 is sorted equal to problem 2
-                                           +1 when problem 1 is sorted after problem 2
-                                usage:  problem1.compareTo(problem2)                  */
+        if((this.building.codePointAt(0)) < (problem2.building.codePointAt(0))){
+            return 1;
+        } else if ((this.building.codePointAt(0)) < (problem2.building.codePointAt(0))) {
+            return -1;
+        }else if((this.building.codePointAt(1)) < (problem2.building.codePointAt(1))){
+            return 1;
+        } else if ((this.building.codePointAt(1)) < (problem2.building.codePointAt(1))) {
+            return -1;
+        } else if((this.building.codePointAt(2)) < (problem2.building.codePointAt(2))){
+            return 1;
+        } else if ((this.building.codePointAt(2)) < (problem2.building.codePointAt(2))) {
+            return -1;
+        } else if ((Integer.getInteger(this.roomNumber)) > (Integer.getInteger(problem2.roomNumber))){
+            return 1;
+        } else if ((Integer.getInteger(this.roomNumber)) < (Integer.getInteger(problem2.roomNumber))){
+            return -1;
+        } else if ((this.errorType.codePointAt(0))>(problem2.errorType.codePointAt(0))){
+            return -1;
+        } else if((this.errorType.codePointAt(0))<(problem2.errorType.codePointAt(0))) {
+            return -1;
+        }else if ((this.errorType.codePointAt(1))>(problem2.errorType.codePointAt(1))){
+            return -1;
+        } else if((this.errorType.codePointAt(1))<(problem2.errorType.codePointAt(1))) {
+            return -1;
+        }
         return 0;
     }
 }
---
+++ src/main/java/ProblemsTypes.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Pdante on 2015-03-14.
+ */
+public enum ProblemsTypes {
+    EmmissionsIssue, ElectricityIssue,WaterIssue
+}
