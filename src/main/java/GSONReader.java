177da3f41c70bfa1765af8d858374dd2e3385cc5
Paul Dante <pdante@users.noreply.github.com>
2015-03-14 20:03:49 -0700

    GSONReader still need location for it to read from

---
+++ src/main/java/GSONReader.java
@@ -0,0 +1,25 @@
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by Pdante on 2015-03-14.
+ */
+public class GSONReader {
+    public GSONReader() {
+        Gson gson = new Gson();
+
+        try {
+
+            BufferedReader br = new BufferedReader(
+                    new FileReader("{'Problem'{)"));
+
+            //convert the json string back to object
+            Problem problem = gson.fromJson(br, Problem.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
