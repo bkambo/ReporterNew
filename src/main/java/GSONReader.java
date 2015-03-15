26bbbaf234f59d52d8995c0639e23be61cd6e217
csemiao <csemiao@gmail.com>
2015-03-14 20:41:18 -0700

    test string

--- src/main/java/GSONReader.java
+++ src/main/java/GSONReader.java
@@ -15,7 +15,27 @@ public class GSONReader {
         try {
 
             BufferedReader br = new BufferedReader(
-                    new StringReader("{'Problem'{)"));
+                    new StringReader("{“entries”:[\n" +
+                            "\t{\n" +
+                            "\t“user”: “features not bugs”,\n" +
+                            "\t“time” : ”04:00”,\n" +
+                            "\t“building” : “MCLD”,\n" +
+                            "\t“roomNumber” : “439”,\n" +
+                            "\t“errorType”: “Water”,  // this should be an ennum\n" +
+                            "“specifics”: “leaky faucet”, // another ennum for each error type\n" +
+                            "“notes”: “yo dawg, this faucet was super leaky”,\n" +
+                            "}\n" +
+                            "{\n" +
+                            "“user”: “A Science Student”,\n" +
+                            "\t“time” : ”04:00”,\n" +
+                            "\t“building” : “BIOL”,\n" +
+                            "\t“roomNumber” : “2000”,\n" +
+                            "\t“errorType”: “Power”,  // this should be an ennum\n" +
+                            "“specifics”: “flickering light”, // another ennum for each error type\n" +
+                            "“notes”: “yo dawg, this light was super flickery”,\n" +
+                            "}\n" +
+                            "]\n" +
+                            "}\n"));
 
             //convert the json string back to object
             Problem problem = gson.fromJson(br, Problem.class);
